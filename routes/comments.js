3f6337550363c0b51d9ce269e97a074f64f6cdd7
Pcjmy <1716045191@qq.com>
2023-01-08 10:13:54 +0800

    koa2 处理http请求

--- routes/comments.js
+++ routes/comments.js
@@ -4,12 +4,28 @@ router.prefix('/api') // 前缀
 
 // 定义路由：模拟获取留言板列表
 router.get('/list', async (ctx) => {
-  ctx.body = 'api list'
+  const query = ctx.query
+  console.log('query', query)
+  // ctx.body = 'api list'
+  ctx.body = {
+    errno: 0,
+    data: [
+      { content: '留言1', user: 'hello1' },
+      { content: '留言2', user: 'hello2' },
+      { content: '留言3', user: 'hello3' }
+    ]
+  }
 })
 
 // 定义路由：模拟创建留言
 router.post('/create', async (ctx) => {
-  ctx.body = 'api create'
+  // ctx.body = 'api create'
+  const body = ctx.request.body // request body
+  console.log('body', body)
+  ctx.body = {
+    errno: 0,
+    message: '成功'
+  }
 })
 
 module.exports = router // 输出
