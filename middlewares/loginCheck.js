fa4d80b177191c6d85e16ae24b7eb88d1def0b6e
Pcjmy <1716045191@qq.com>
2023-03-29 17:51:30 +0800

    feat: 登录验证的中间件

---
+++ middlewares/loginCheck.js
@@ -0,0 +1,17 @@
+// 登录验证的中间件
+
+async function loginCheck(ctx, next) {
+  const userInfo = ctx.session.userInfo;
+  if (userInfo && userInfo.username) {
+    // 登录验证成功
+    await next()
+    return
+  }
+  // 登录失败
+  ctx.body = {
+    errno: -1,
+    message: '请登录'
+  }
+}
+
+module.exports = loginCheck
