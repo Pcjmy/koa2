c2e224a280bf01221a0e8de39b8286a848dcb970
Pcjmy <1716045191@qq.com>
2023-03-29 20:46:20 +0800

    feat: 模拟登录

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,26 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+// 模拟登录
+router.get('/login-mock', async (ctx, next) => {
+  let str = ''
+  const query = ctx.query // url参数，querystring
+  if (query.username) {
+    // 模拟登录成功
+    ctx.session.userInfo = {
+      username: query.username
+    }
+    str = 'login ok'
+  } else {
+    // 模拟登录失败，不用处理session
+    str = 'login failed'
+  }
+  ctx.body = str
+})
+
+// 模拟登录验证
+router.get('/login-check-mock', async (ctx, next) => {
+  ctx.body = ctx.session.userInfo || {}
+})
+
 module.exports = router
